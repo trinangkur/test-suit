e198851534666a60ab0210cc00f36c46978f9ba6
Trinangkur Chatterjee <58026183+trinangkur@users.noreply.github.com>
2020-07-01 17:20:42 +0530

    instead of talking with http server worker is talking to the db directly

--- worker.js
+++ worker.js
@@ -1,20 +1,55 @@
-const https = require('https');
-const runTest = require('./src/runTest');
-const { getJson, getOption } = require('./src/util');
+const client = require('redis').createClient(process.env.REDIS_URL);
+const { exec, execSync } = require('child_process');
+
+const getUserDetails = function (userName) {
+  return new Promise((resolve, rejects) => {
+    client.hget('users', userName, (err, userDetails) => {
+      resolve(JSON.parse(userDetails));
+    });
+  });
+};
+
+const runExec = function ({ link, sha, pushedAt }) {
+  return new Promise((resolve) => {
+    exec(
+      `rm -rf suit && git clone ${link} suit && cd suit && git checkout ${sha} && npm install && npm test`,
+      (err) => {
+        execSync('rm -rf suit');
+        resolve({ sha, pushedAt, status: err === null });
+      }
+    );
+  });
+};
+
+const addLog = function (userName, repoName, log) {
+  return new Promise((resolve, reject) => {
+    getUserDetails(userName).then((userDetails) => {
+      userDetails = userDetails || {};
+      userDetails[repoName] || (userDetails[repoName] = []);
+      userDetails[repoName].push(log);
+      client.hset('users', userName, JSON.stringify(userDetails), resolve);
+    });
+  });
+};
+
+const runTest = function (jobDetails) {
+  const { repoName, userName } = jobDetails;
+  console.log('running test for-->', jobDetails);
+  return new Promise((resolve, reject) => {
+    runExec(jobDetails)
+      .then(addLog.bind(null, userName, repoName))
+      .then(resolve);
+  });
+};
 
 const pullJob = function () {
-  const options = getOption();
-  options.path = '/rpop/0/jobs';
-  https.get(options, (res) => {
-    if (res.headers['content-type'] === 'application/json; charset=utf-8') {
-      getJson(res).then(({ value }) => {
-        if (value) {
-          runTest(JSON.parse(value)).then(pullJob);
-        } else {
-          console.log('waiting');
-          setTimeout(pullJob, 5000);
-        }
-      });
+  client.brpop('jobs', 4, (err, job) => {
+    if (job) {
+      console.log('got job -->', job[1]);
+      runTest(JSON.parse(job[1])).then(pullJob);
+    } else {
+      console.log("didn't got any job");
+      pullJob();
     }
   });
 };
